46b742374278731722d2df41c82ddff429daa3e3
georgetoothman <george.toothman@gmail.com>
2014-04-29 16:50:14 -0400

    added .ds_store to gitignore

---
+++ georgetoothman.com/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GeorgetoothmanCom::Application.config.secret_key_base = 'a901084899fe766dd13a3bf574fa50bf3cced363daf125ff60bac4be229046eb42c273e46db1cf98e1ed171015126648fdd7445bd2a3815fbc1e60258f023d71'
